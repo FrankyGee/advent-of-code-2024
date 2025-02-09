70952b72bf048f5fd0dabec53cd3179f7284eae0
FrankyGee <frankygeorg@gmail.com>
2025-02-09 15:23:32 +1030

    Begin Day08

---
+++ src/main/kotlin/Day08.kt
@@ -0,0 +1,12 @@
+class Day08 {
+
+    fun part1(input: List<String>): Long {
+        return 0
+    }
+
+    fun part2(input: List<String>): Long {
+        return 0
+    }
+
+
+}
---
+++ src/test/kotlin/Day08Test.kt
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.expect
+
+class Day08Test {
+    private val day = "08"
+
+    private val testInput = readInput("Day${day}_test")
+    private val input = readInput("Day${day}")
+    private var sut = Day08()
+
+    @Test
+    fun `Day 8 Part 1 test`() {
+        expect(14) {
+            sut.part1(testInput)
+        }
+    }
+
+    @Test
+    fun `Day 8 Part 2 test`() {
+        expect(-1) {
+            sut.part2(testInput)
+        }
+    }
+
+    @Test
+    fun `Day 8 Part 1 actual`() {
+        expect(-1) {
+            sut.part1(input)
+        }
+    }
+
+    @Test
+    fun `Day 8 Part 2 actual`() {
+        expect(-1) {
+            sut.part2(input)
+        }
+    }
+
+}
